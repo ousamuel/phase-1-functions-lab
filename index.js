7bba8347f9872da9287049c7ba34a47ef191fbd5
Samuel <samuelou510@gmail.com>
2023-06-12 15:33:06 -0400

    functions lab complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks){
+  return Math.abs(blocks - 42);
+}
+
+function distanceFromHqInFeet(blocks){
+  return distanceFromHqInBlocks(blocks) * 264
+}
+
+function distanceTravelledInFeet(start, end){
+
+  return Math.abs(start - end) * 264;
+}
+
+function calculatesFarePrice(start, end){
+  if (distanceTravelledInFeet(start, end) < 400){
+    return 0;
+  } else if (distanceTravelledInFeet(start, end) > 400 && distanceTravelledInFeet(start, end) < 2000) {
+    return 0.02 * (distanceTravelledInFeet(start, end) - 400);
+  } else if (distanceTravelledInFeet(start, end) >=2000 && distanceTravelledInFeet(start, end) <= 2500){
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
